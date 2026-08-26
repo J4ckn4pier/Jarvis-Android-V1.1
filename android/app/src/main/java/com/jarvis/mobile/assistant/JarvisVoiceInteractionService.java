19e2de55b1dd40b91eb63c7e97feb1aae529ce85
J4ckn4pier <charlesmillerofficial7@gmail.com>
2026-08-26 16:00:02 -0600

    Retry 1: plain Android source build

---
+++ android/app/src/main/java/com/jarvis/mobile/assistant/JarvisVoiceInteractionService.java
@@ -0,0 +1,1 @@
+package com.jarvis.mobile.assistant; import android.service.voice.VoiceInteractionService; public class JarvisVoiceInteractionService extends VoiceInteractionService {}
---
+++ android/app/src/main/java/com/jarvis/mobile/assistant/JarvisVoiceSessionService.java
@@ -0,0 +1,1 @@
+package com.jarvis.mobile.assistant; import android.service.voice.*; public class JarvisVoiceSessionService extends VoiceInteractionSessionService { public VoiceInteractionSession onNewSession(android.os.Bundle b){return new JarvisVoiceSession(this);} }
