c8970b1c9bf0d2ced880d8c73ce97fec4ff6eb57
J4ckn4pier <charlesmillerofficial7@gmail.com>
2026-08-27 16:00:28 -0600

    Trace voice interaction service activation

--- android/app/src/main/java/com/jarvis/mobile/assistant/JarvisVoiceInteractionService.java
+++ android/app/src/main/java/com/jarvis/mobile/assistant/JarvisVoiceInteractionService.java
@@ -1,2 +1,21 @@
-package com.jarvis.mobile.assistant; import android.service.voice.VoiceInteractionService; public class JarvisVoiceInteractionService extends VoiceInteractionService {}
+package com.jarvis.mobile.assistant;
 
+import android.service.voice.VoiceInteractionService;
+import android.util.Log;
+
+/** Android system entry point for the active JARVIS voice interaction service. */
+public class JarvisVoiceInteractionService extends VoiceInteractionService {
+    private static final String TEST_TAG = "JARVIS_ASSISTANT_TEST";
+
+    @Override
+    public void onReady() {
+        super.onReady();
+        Log.i(TEST_TAG, "JARVIS_VOICE_SERVICE_READY");
+    }
+
+    @Override
+    public void onShutdown() {
+        Log.i(TEST_TAG, "JARVIS_VOICE_SERVICE_SHUTDOWN");
+        super.onShutdown();
+    }
+}
