abd76f16eb125a0d8217e135212783e3951e7d3b
J4ckn4pier <charlesmillerofficial7@gmail.com>
2026-08-27 16:50:49 -0600

    Trace assistant session service creation

--- android/app/src/main/java/com/jarvis/mobile/assistant/JarvisVoiceSessionService.java
+++ android/app/src/main/java/com/jarvis/mobile/assistant/JarvisVoiceSessionService.java
@@ -1,2 +1,17 @@
-package com.jarvis.mobile.assistant; import android.service.voice.*; public class JarvisVoiceSessionService extends VoiceInteractionSessionService { public VoiceInteractionSession onNewSession(android.os.Bundle b){return new JarvisVoiceSession(this);} }
+package com.jarvis.mobile.assistant;
 
+import android.os.Bundle;
+import android.service.voice.VoiceInteractionSession;
+import android.service.voice.VoiceInteractionSessionService;
+import android.util.Log;
+
+/** Creates the actual JARVIS assistant session after Android routes an assist invocation. */
+public class JarvisVoiceSessionService extends VoiceInteractionSessionService {
+    private static final String TEST_TAG = "JARVIS_ASSISTANT_TEST";
+
+    @Override
+    public VoiceInteractionSession onNewSession(Bundle args) {
+        Log.i(TEST_TAG, "JARVIS_SESSION_SERVICE_NEW_SESSION");
+        return new JarvisVoiceSession(this);
+    }
+}
